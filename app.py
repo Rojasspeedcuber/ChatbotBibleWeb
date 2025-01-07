0c392de99deb62532eed4986d19545fff94cf83d
Henrique Rojas <henriquerojinhas@gmail.com>
2025-01-06 22:11:34 -0300

    Foi!!

--- app.py
+++ app.py
@@ -4,32 +4,9 @@ from langchain import hub
 from decouple import config
 from langchain_groq import ChatGroq
 from langchain.agents import create_react_agent, AgentExecutor
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
-
-
-def ask_question(model, query):
-    llm = ChatGroq(model=model)
-    system_prompt = '''
-    Use o contexto para responder as perguntas.
-    Se não encontrar uma resposta no contexto,
-    explique que não há informações disponíveis.
-    Responda em formato de markdown e com visualizações
-    elaboradas e interativas.
-    Use como base a Bíblia Sagrada disponibilizada no banco de dados.
-    A resposta final deve ter uma formatação amigável(markdown) de vizualização para o usuário.
-    Responda sempre em português brasileiro.
-    Contexto: {context}
-    '''
-
-    messages = [('system', system_prompt, agent)]
-    for message in st.session_state.messages:
-        messages.append((message.get('role'), message.get('content')))
-    messages.append(('human', '{input}'))
-
-    response = agent.invoke({'input': 'query'})
-    return response.get('answer')
 
 
 os.environ['GROQ_API_KEY'] = config('GROQ_API_KEY')
@@ -48,7 +25,7 @@ model_options = [
     'mixtral-8x7b-32768',
 ]
 
-selected_model = st.sidebar.selectbox(
+selected_box = st.sidebar.selectbox(
     label='Selecione o modelo LLM',
     options=model_options,
 )
@@ -63,7 +40,7 @@ if 'messages' not in st.session_state:
 user_question = st.chat_input('O que deseja saber sobre a Bíblia?')
 
 model = ChatGroq(
-    model=selected_model,
+    model=selected_box,
 )
 
 
@@ -97,11 +74,14 @@ prompt = '''
 prompt_template = PromptTemplate.from_template(prompt)
 
 
-if st.button('Enviar'):
-    if user_question:
-        with st.spinner('Consultando banco de dados...'):
-            formatted_prompt = prompt_template.format(q=user_question)
-            output = agent_executor.invoke({'input': formatted_prompt})
-            st.markdown(output.get('output'))
-    else:
-        st.warning('Por favor, insira uma pergunta.')
+if user_question:
+    for message in st.session_state.messages:
+        st.chat_message(message.get('role')).write(message.get('content'))
+
+    st.chat_message('user').write(user_question)
+    st.session_state.messages.append(
+        {'role': 'user', 'content': user_question})
+    with st.spinner('Buscando resposta...'):
+        formatted_prompt = prompt_template.format(q=user_question)
+        output = agent_executor.invoke({'input': formatted_prompt})
+        st.markdown(output.get('output'))
