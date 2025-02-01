ff4ab35efe0a4839f6289c622e36407ca483fbff
Henrique Rojas <henriquerojinhas@gmail.com>
2025-02-01 11:42:11 -0300

    tentativa

--- api/apimercadopago.py
+++ api/apimercadopago.py
@@ -19,16 +19,9 @@ def gerar_link_pagamento():
     payment_response = sdk.payment().create(payment_data, request_options)
     payment = payment_response["response"]
 
-    # Novo caminho para acessar transaction_data
     transaction = payment.get("point_of_interaction",
                               {}).get("transaction_data", {})
 
-    if not transaction:
-        raise KeyError("transaction_data não encontrado na resposta da API")
-
     link_iniciar_pagamento = transaction.get("ticket_url")
-
-    if not link_iniciar_pagamento:
-        raise KeyError("ticket_url não encontrado na resposta da API")
 
     return link_iniciar_pagamento
