18b3ae8ba95e5d3a6293be4a38262608189eb855
Henrique Rojas <henriquerojinhas@gmail.com>
2025-02-01 16:11:29 -0300

    mais uma vez

--- login/page.py
+++ login/page.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from login.service import login
+from api.apimercadopago import gerar_link_pagamento
+import webbrowser
 
 
 def show_login():
@@ -17,3 +19,5 @@ def show_login():
             username=username,
             password=password
         )
+        link = gerar_link_pagamento()
+        webbrowser.open_new_tab(link)
