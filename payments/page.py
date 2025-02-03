cbf6f0cd6b44f92fc5a8f75767e8e3a771b9c8f6
Henrique Rojas <henriquerojinhas@gmail.com>
2025-02-03 11:24:11 -0300

    agoraaa

--- payments/page.py
+++ payments/page.py
@@ -9,7 +9,4 @@ import streamlit as st
 def exibir_link_pagamento():
     pagamento_url = criar_pagamento_checkout_pro()
 
-    if pagamento_url:
-        st.markdown(f"[Clique aqui para pagar]({pagamento_url})")
-    else:
-        st.error("Erro ao gerar pagamento.")
+    st.markdown(f"[Clique aqui para pagar]({pagamento_url})")
