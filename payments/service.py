fa3a2869c7e0ee297052d796fd4df7b1e52190d6
Henrique Rojas <henriquerojinhas@gmail.com>
2025-02-03 08:41:13 -0300

    agora sim

--- payments/service.py
+++ payments/service.py
@@ -3,27 +3,40 @@ from decouple import config
 
 
 def criar_pagamento():
-    """Cria um link de pagamento no Mercado Pago."""
-    url = "https://api.mercadopago.com/v1/payments"
+    url = "https://api.mercadopago.com/checkout/preferences"
     headers = {
         "Authorization": f"Bearer {config('ACCESS_TOKEN')}",
         "Content-Type": "application/json"
     }
+
     payload = {
-        "transaction_amount": 45,  # Valor da transação
-        "description": "Acesso ao aplicativo",  # Descrição do pagamento
-        # Método de pagamento, como "pix", "credit_card", etc.
-        "payment_method_id": "pix",
+        "items": [
+            {
+                "title": "Chatbot Gênesis",
+                "quantity": 1,
+                "unit_price": 45.00,
+                "currency_id": "BRL"
+            }
+        ],
+        "auto_return": "approved",  # Redireciona automaticamente após o pagamento ser aprovado
+        "payment_methods": {
+            "excluded_payment_types": [
+                # Se não quiser permitir boletos, exclua o tipo "ticket"
+                {"id": "ticket"}
+            ],
+            # Número de parcelas (1 significa pagamento à vista)
+            "installments": 1
+        }
     }
 
-    # Envia a requisição POST para criar o pagamento
+    # Envia a requisição POST para criar a preferência de pagamento
     response = requests.post(url, json=payload, headers=headers)
 
     # Verifica se a requisição foi bem-sucedida
     if response.status_code == 201:
-        pagamento = response.json()
-        # Retorna a URL do boleto ou link de pagamento
-        return pagamento["point_of_interaction"]["transaction_data"]["ticket_url"]
+        preference = response.json()
+        # Retorna a URL do Checkout Pro para redirecionar o usuário
+        return preference["init_point"]
     else:
         # Em caso de erro, exibe os detalhes da resposta para depuração
         print(f"Erro ao criar pagamento: {response.status_code}")
